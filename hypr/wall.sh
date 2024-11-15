8fbdd7d77fcddc43999a97ffa1c6f6013df15c84
Celeste Ornato <celeste.ornato1@gmail.com>
2024-11-15 23:28:02 +0100

    fixed waybar not starting

--- hypr/wall.sh
+++ hypr/wall.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+cp ~/.cache/bgimage "~/Pictures/Wallpaper/$(ls -1 ~/Pictures/Wallpapers | wc -l | awk '{print $1}')-bg"
 cp -f $(find ~/Pictures/Wallpaper -type f | shuf -n1) ~/.cache/bgimage
 swaybg -i ~/.cache/bgimage
